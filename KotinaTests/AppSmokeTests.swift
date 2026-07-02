1386c4687b3831faef240647dd97b7acf32f4d10
littleduck <107936957+littleduck1219@users.noreply.github.com>
2026-07-02 14:11:48 +0900

    build: target macOS 15 for local translation

--- KotinaTests/AppSmokeTests.swift
+++ KotinaTests/AppSmokeTests.swift
@@ -5,4 +5,11 @@ final class AppSmokeTests: XCTestCase {
     func testApplicationNameIsKotina() {
         XCTAssertEqual(AppIdentity.name, "Kotina")
     }
+
+    func testMinimumSystemVersionIsMacOS15() {
+        XCTAssertEqual(
+            Bundle.main.object(forInfoDictionaryKey: "LSMinimumSystemVersion") as? String,
+            "15.0"
+        )
+    }
 }
